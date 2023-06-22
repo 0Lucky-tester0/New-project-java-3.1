b0374032760a3afaf93a3cd50a5725ed262991f4
Emma Paris <Egorsovetskiy88@gmail.com>
2023-06-22 19:05:30 +0300

    l commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,7 +2,7 @@ public class BonusMilesService {
     public int calculate(int price) {
 
         int bonus = 20; // стоимость одной мили
-        int miles = price/bonus; // формула расчета бонусных миль
+        int miles = price / bonus; // формула расчета бонусных миль
 
         return miles;
 
